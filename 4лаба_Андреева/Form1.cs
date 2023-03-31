44a9abbb562aa35471b8e5b0855a93aaa6d39b05
OxiD1Zed <skabo1000@mail.ru>
2023-03-31 23:18:57 +0300

    Начальный код

---
+++ 4лаба_Андреева/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _4лаба_Андреева
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Program.RefreshColor(button1, textBox1);
+            Program.ReplaceTextOfButton(button1, textBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Program.RefreshColor(button2, textBox1);
+            Program.ReplaceTextOfButton(button2, textBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Program.RefreshColor(button3, textBox1);
+            Program.ReplaceTextOfButton(button3, textBox1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Program.RefreshColor(button4, textBox1);
+            Program.ReplaceTextOfButton(button4, textBox1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Program.RefreshColor(button5, textBox1);
+            Program.ReplaceTextOfButton(button5, textBox1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Program.RefreshColor(button6, textBox1);
+            Program.ReplaceTextOfButton(button6, textBox1);
+        }
+    }
+}
---
+++ 4лаба_Андреева/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _4лаба_Андреева
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static void ReplaceTextOfButton(Button b, TextBox t)
+        {
+            b.Text = t.Text;
+        }
+
+        public static void RefreshColor(Button b, TextBox t)
+        {
+            Dictionary<string, Color> colors = new Dictionary<string, Color>() {
+                { "красный", Color.Red },
+                { "оранжевый", Color.Orange },
+                {"желтый", Color.Yellow },
+                {"зеленый", Color.Green },
+                {"синий", Color.Blue },
+                {"голубой", Color.SkyBlue },
+                {"фиолетовый", Color.Purple }
+            };
+
+            string color = t.Text.ToLower().Trim();
+
+            if (colors.ContainsKey(color))
+            {
+                b.BackColor = colors[color];
+            }
+        }
+    }
+}
